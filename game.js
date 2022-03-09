f47aec1623cc73bfe201c00700dacd83af568560
Angela Sun <angelasun1995@gmail.com>
2022-03-08 23:21:46 -0500

    Add playRound function

--- game.js
+++ game.js
@@ -3,8 +3,6 @@ function computerPlay() {
 
     const option = getRandomInt(3);
 
-    console.log(option);
-
     if (option === 0){
         return 'Rock'
     } 
@@ -15,3 +13,48 @@ function computerPlay() {
         return 'Scissors'
     }
 }
+
+function playRound(playerSelection, computerSelection){
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
+
+    console.log(`Player played: ${player}`);
+    console.log(`Computer played: ${computer}`);
+
+    if (player == 'rock'){
+        if (computer == 'rock'){
+            return "It's a tie! You both had Rock"
+        }
+        if (computer == 'scissors'){
+            return 'You win! Rock beats Scissors'
+        }
+        if (computer == 'paper'){
+            return 'You lose! Paper beats Rock'
+        }
+    }
+    if (player == 'scissors'){
+        if (computer == 'rock'){
+            return "You lose! Rock beats Scissors"
+        }
+        if (computer == 'scissors'){
+            return "It's a tie! You both had Scissors"
+        }
+        if (computer == 'paper'){
+            return 'You win! Scissors beats Paper'
+        }
+    }
+    if (player == 'paper'){
+        if (computer == 'rock'){
+            return "You win! Paper beats Rock"
+        }
+        if (computer == 'scissors'){
+            return "You lose! Scissors beats Paper"
+        }
+        if (computer == 'paper'){
+            return "It's a tie! You both had Paper"
+        }
+    }
+}
+
+console.log(playRound('rock', computerPlay()));
+
