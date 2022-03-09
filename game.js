7ad6d86e5aecd9fb6a25881b1265b835c6b4f07d
Angela Sun <angelasun1995@gmail.com>
2022-03-08 22:57:48 -0500

    Add computerPlay function

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
-console.log('I\'m connected!')
+function computerPlay() {
+    const getRandomInt = max => Math.floor(Math.random() * max)
+
+    const option = getRandomInt(3);
+
+    console.log(option);
+
+    if (option === 0){
+        return 'Rock'
+    } 
+    if (option === 1){
+        return 'Paper'
+    }
+    if (option === 2){
+        return 'Scissors'
+    }
+}
